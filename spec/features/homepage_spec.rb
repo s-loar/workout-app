283094e3a00d187547ae3aeacf39b6bfc1e8321a
Steven Loar <sjl7909@gmail.com>
2017-05-19 15:53:38 -0400

    Homepage test passes

--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -5,8 +5,8 @@ RSpec.feature "Display Home Page" do
   scenario "when user visits the home page" do
     visit '/'
 
-    expect(page).to have_link('Athletes Den')
     expect(page).to have_link('Home')
+    expect(page).to have_link('Athletes Den')
     expect(page).to have_content('Workout Lounge')
     expect(page).to have_content('Show off your workout to your friends and family')
   end
