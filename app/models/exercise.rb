cae9a2df3622d333cd7aa0aac59e487707b002f1
Steven Loar <sjl7909@gmail.com>
2017-06-08 11:12:51 -0400

    Implement exercise model validation

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,8 +1,12 @@
 class Exercise < ApplicationRecord
   belongs_to :user
 
-  validates :duration_in_minutes, numericality: true
-  validates :workout, presence: true
-  validates :workout_date, presence: true
+  alias_attribute :workout_details, :workout
+  alias_attribute :duration, :duration_in_minutes
+  alias_attribute :activity_date, :workout_date
+
+  validates :duration, numericality: { greater_than: 0.0 }
+  validates :workout_details, presence: true
+  validates :activity_date, presence: true
 
 end
--- spec/features/exercises/creating_exercises_spec.rb
+++ spec/features/exercises/creating_exercises_spec.rb
@@ -5,15 +5,16 @@ RSpec.feature "Creating Exercises" do
   before do
     @john = User.create!(email: "john@example.com", password: "password")
     login_as(@john)
-  end
 
-  scenario "successfully create an exercise" do
     visit "/"
     click_link "My Lounge"
     expect(page).to have_link("New Workout")
 
     click_link "New Workout"
     expect(page).to have_link("Back")
+  end
+
+  scenario "successfully create an exercise" do
 
     fill_in "Duration", with: 70
     fill_in "Workout Details", with: "Weight lifting"
@@ -28,12 +29,6 @@ RSpec.feature "Creating Exercises" do
   end
 
   scenario "fails to create an exercise" do
-    visit "/"
-    click_link "My Lounge"
-    expect(page).to have_link("New Workout")
-
-    click_link "New Workout"
-    expect(page).to have_link("Back")
 
     fill_in "Duration", with: ""
     fill_in "Workout Details", with: ""
@@ -41,9 +36,9 @@ RSpec.feature "Creating Exercises" do
     click_button "Create Exercise"
 
     expect(page).to have_content("Exercise has not been created")
-    expect(page).to have_content("Duration in minutes is not a number")
-    expect(page).to have_content("Workout can't be blank")
-    expect(page).to have_content("Workout date can't be blank")
+    expect(page).to have_content("Duration is not a number")
+    expect(page).to have_content("Workout details can't be blank")
+    expect(page).to have_content("Activity date can't be blank")
   end
 
 end
