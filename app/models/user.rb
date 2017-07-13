741a35b34648c0bdb8be8b529175bb8f8d80a4ab
Steven Loar <sjl7909@gmail.com>
2017-07-13 11:38:33 -0400

    Add friendship and creating followings

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :exercises
+  has_many :friendships
+  has_many :friends, through: :friendships, class_name: "User"
 
   validates :first_name, presence: true
   validates :last_name, presence: true
@@ -25,6 +27,10 @@ class User < ApplicationRecord
         "%#{names_array[0]}%", "%#{names_array[1]}%",
         "%#{names_array[0]}%", "%#{names_array[1]}%").order(:first_name)
     end
+  end
+
+  def follows_or_same?( athlete )
+    friendships.map(&:friend).include?( athlete ) || self == athlete
   end
 
 end
