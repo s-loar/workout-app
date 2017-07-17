4359c4eeb733617e927b37f9e9956b3a12a4182e
Steven Loar <sjl7909@gmail.com>
2017-07-17 14:00:58 -0400

    Implement chatroom setup

--- spec/features/users/signing_up_users_spec.rb
+++ spec/features/users/signing_up_users_spec.rb
@@ -12,7 +12,11 @@ RSpec.feature "Signing Up Users" do
     fill_in "Password confirmation", with: "password"
     click_button "Sign up"
     expect(page).to have_content("You have signed up successfully.")
-    # expect(page.current_path).to eq(articles_path)
+
+    user = User.last
+    room = user.room
+    room_name = user.full_name.split(' ').join('-')
+    expect(room.name).to eq(room_name)
 
     visit "/"
     expect(page).to have_content("John Doe")
